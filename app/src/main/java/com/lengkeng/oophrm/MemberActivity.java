be792be29d69d878995db5beb10345029c525a9d
Mai PL <lanmai1995@gmail.com>
2016-04-22 23:16:29 +0700

    update quan ly nhan vien

--- app/src/main/java/com/lengkeng/oophrm/MemberActivity.java
+++ app/src/main/java/com/lengkeng/oophrm/MemberActivity.java
@@ -1,5 +1,6 @@
 package com.lengkeng.oophrm;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -8,8 +9,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.lengkeng.oophrm.fragments.ListMemberFragment;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MemberActivity extends AppCompatActivity {
     public static int count = 0;
--- app/src/main/java/com/lengkeng/oophrm/models/Employee.java
+++ app/src/main/java/com/lengkeng/oophrm/models/Employee.java
@@ -1,15 +1,37 @@
 package com.lengkeng.oophrm.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Le Vinh Thien on 4/8/2016.
  * Contact: levinhthien.bka@gmail.com
  */
 public class Employee {
+    private  String id;
     private String name;
     private String chucvu;
+    private Integer id_group;
 
-    public Employee(String name, String chucvu) {
+    public Employee(String id, String name, String chucvu, Integer group) {
+        this.id = id;
         this.chucvu = chucvu;
+        this.name = name;
+        this.id_group = group;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
@@ -21,11 +43,11 @@ public class Employee {
         this.chucvu = chucvu;
     }
 
-    public String getName() {
-        return name;
+    public Integer getId_group() {
+        return id_group;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId_group(Integer id_group) {
+        this.id_group = id_group;
     }
 }
