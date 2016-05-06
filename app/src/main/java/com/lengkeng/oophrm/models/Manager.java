2f9da23a56e20e6654e53315af92aa118a118ae9
Lê Vĩnh Thiện <ohmygodvt95@gmail.com>
2016-05-07 00:01:11 +0700

    Hoàn thiện class Manager

--- app/src/main/java/com/lengkeng/oophrm/models/Manager.java
+++ app/src/main/java/com/lengkeng/oophrm/models/Manager.java
@@ -1,9 +1,21 @@
 package com.lengkeng.oophrm.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Le Vinh Thien on 5/1/2016.
  * Contact: levinhthien.bka@gmail.com
  */
-public class Manager {
+public class Manager extends Employee{
     private int bonus;
+
+    public Manager(JSONObject obj){
+        super(obj);
+        try {
+            this.bonus = obj.has("bonus")? obj.getInt("bonus"): 0;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
