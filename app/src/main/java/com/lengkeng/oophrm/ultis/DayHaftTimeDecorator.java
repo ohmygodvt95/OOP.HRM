2fbe80cc87a178624ecfd83db9b14a116dc60a35
Lê Vĩnh Thiện <ohmygodvt95@gmail.com>
2016-05-13 16:37:12 +0700

    Thêm hiệu ứng ngày cho calendar

---
+++ app/src/main/java/com/lengkeng/oophrm/ultis/DayHaftTimeDecorator.java
@@ -0,0 +1,42 @@
+package com.lengkeng.oophrm.ultis;
+
+import android.graphics.Color;
+
+import com.lengkeng.oophrm.models.Day;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+
+/**
+ * Created by Le Vinh Thien on 5/13/2016.
+ * Contact: levinhthien.bka@gmail.com
+ */
+public class DayHaftTimeDecorator implements DayViewDecorator {
+    private static final DateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+    private int color;
+    private HashMap<String, Day> dates;
+
+    public DayHaftTimeDecorator(HashMap<String, Day> dates) {
+        this.color = Color.parseColor("#53BBF4");
+        this.dates = dates;
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        if(dates.containsKey(FORMATTER.format(day.getDate())) ){
+            return dates.get(FORMATTER.format(day.getDate())).getTitle().compareTo("nửa ngày") == 0;
+        }
+        return false;
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new DotSpan(10, color));
+    }
+}
---
+++ app/src/main/java/com/lengkeng/oophrm/ultis/OneDayDecorator.java
@@ -0,0 +1,44 @@
+package com.lengkeng.oophrm.ultis;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Parcelable;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Date;
+
+/**
+ * Decorate a day by making the text big and bold
+ */
+public class OneDayDecorator implements DayViewDecorator {
+
+    private CalendarDay date;
+
+    public OneDayDecorator() {
+        date = CalendarDay.today();
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return date != null && day.equals(date);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new StyleSpan(Typeface.BOLD_ITALIC));
+    }
+
+    /**
+     * We're changing the internals, so make sure to call {@linkplain MaterialCalendarView#invalidateDecorators()}
+     */
+    public void setDate(Date date) {
+        this.date = CalendarDay.from(date);
+    }
+}
